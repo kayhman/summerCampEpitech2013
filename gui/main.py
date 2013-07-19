257c674c34621c73735ce2012262c79025a0250b
Guillaume <guillaume.saupin@gmail.com>
2013-07-20 00:15:45 +0200

    Promote epitech.py to main.py

--- gui/main.py
+++ gui/main.py
@@ -1,7 +1,20 @@
-from kivy.app import App
-from kivy.clock import Clock
-from kivy.core.window import Window
+import kivy
+kivy.require('1.1.1')
+
+from kivy.lang import Builder
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.label import Label
+from kivy.app import App
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.graphics import Color, Line, Rectangle, Ellipse
+from kivy.clock import Clock
+
+from jnius import autoclass
+from time import sleep, time
+
+from kivy.core.window import Window
 from kivy.resources import resource_find
 from kivy.graphics.transformation import Matrix
 from kivy.graphics.opengl import *
@@ -9,9 +22,9 @@ from kivy.graphics import *
 from objloader import ObjFile
 import fastObjLoader
 
-from time import sleep, time
 
 class Renderer(Widget):
+    layout = ObjectProperty()
     def __init__(self, **kwargs):
         self.canvas = RenderContext(compute_normal_mat=True)
         self.canvas.shader.source = resource_find('simple.glsl')
@@ -23,6 +36,7 @@ class Renderer(Widget):
             PopMatrix()
             self.cb = Callback(self.reset_gl_context)
         Clock.schedule_interval(self.update_glsl, 1 / 60.)
+	self.rotation = 1
 
     def setup_gl_context(self, *args):
         glEnable(GL_DEPTH_TEST)
@@ -36,7 +50,7 @@ class Renderer(Widget):
         self.canvas['projection_mat'] = proj
         self.canvas['diffuse_light'] = (1.0, 1.0, 0.8)
         self.canvas['ambient_light'] = (0.1, 0.1, 0.1)
-        self.rot.angle += 1
+        self.rot.angle += 1 * self.rotation
 
     def setup_scene_fast(self):
         Color(1, 1, 1, 1)
@@ -74,11 +88,29 @@ class Renderer(Widget):
             mode='triangles',
         )
         PopMatrix()
+    
+    def invert_rotation(self, dummy):
+	self.rotation = self.rotation * -1
 
+class Epitech(Screen):
+    noteLabel = ObjectProperty()
+    invertButton = ObjectProperty()
+    volume = ObjectProperty()
+    renderer = ObjectProperty()
 
-class RendererApp(App):
     def build(self):
-        return Renderer()
+      self.noteLabel.text = "No key pressed"
+      self.invertButton.bind(on_press=self.renderer.invert_rotation)
 
-if __name__ == "__main__":
-    RendererApp().run()
+class EpitechApp(App):
+    def build(self):
+        self.sm = ScreenManager()
+        self.game = Epitech(name="game")
+        self.game.build()
+        self.sm.add_widget(self.game)
+	self.sm.curent = "game"
+        print "build done"
+        return self.sm
+
+if __name__ == '__main__':
+    EpitechApp().run()
